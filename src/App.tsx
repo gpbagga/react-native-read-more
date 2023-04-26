9e5fd3e66a1c5fbb7de1e222263cf73611f0eb9a
Gaganpreet Singh Bagga <gpbagga1234@gmail.com>
2023-04-27 00:54:39 +0530

    React imported in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import ReadMoreMain from "./helpers/ReadMoreMain";
 const App = (props) => {
